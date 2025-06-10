cc829b1d63a6a4ea107eec843911fd690e1a9f4b
Chao Fei <devcfei@outlook.com>
2025-06-10 09:56:32 +0800

    add custom sample

---
+++ samples/custom/custom_packet.hpp
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <string>
+#include <memory>
+#include <netp.h>
+
+namespace netp {
+
+// Command definitions
+enum class CustomCommand : uint32_t {
+    HELLO = 1,          // Initial handshake
+    DATA = 2,           // Regular data transfer
+    WARNING = 3,        // Warning response for undefined commands
+    KILL = 4,          // Kill notification before termination
+    UNDEFINED = 0xFFFFFFFF
+};
+
+// Packet header structure
+#pragma pack(push, 1)
+struct CustomPacketHeader {
+    uint32_t length;    // Total packet length including header
+    uint32_t id;        // Client ID
+    uint32_t cmd;       // Command type
+};
+#pragma pack(pop)
+
+class CustomPacket : public BasicPacket<CustomPacket> {
+public:
+    CustomPacket() : header_({0, 0, 0}), data_() {}
+    
+    // Constructor for creating a packet
+    CustomPacket(uint32_t id, CustomCommand cmd) {
+        header_.id = id;
+        header_.cmd = static_cast<uint32_t>(cmd);
+        header_.length = sizeof(CustomPacketHeader);
+    }
+
+    // Setters
+    void setData(const std::vector<uint8_t>& data) {
+        data_ = data;
+        header_.length = sizeof(CustomPacketHeader) + data_.size();
+    }
+
+    void setId(uint32_t id) { header_.id = id; }
+    void setCommand(CustomCommand cmd) { header_.cmd = static_cast<uint32_t>(cmd); }
+
+    // Getters
+    uint32_t getId() const { return header_.id; }
+    CustomCommand getCommand() const { return static_cast<CustomCommand>(header_.cmd); }
+    uint32_t getLength() const { return header_.length; }
+    const std::vector<uint8_t>& getData() const { return data_; }
+
+    // Implementation of BasicPacket interface
+    std::vector<uint8_t> serializeImpl() const {
+        std::vector<uint8_t> buffer(header_.length);
+        memcpy(buffer.data(), &header_, sizeof(CustomPacketHeader));
+        if (!data_.empty()) {
+            memcpy(buffer.data() + sizeof(CustomPacketHeader), data_.data(), data_.size());
+        }
+        return buffer;
+    }
+
+    bool deserializeImpl(const uint8_t* data, size_t length) {
+        if (length < sizeof(CustomPacketHeader)) {
+            return false;
+        }
+
+        memcpy(&header_, data, sizeof(CustomPacketHeader));
+        
+        if (length != header_.length) {
+            return false;
+        }
+
+        if (header_.length > sizeof(CustomPacketHeader)) {
+            data_.resize(header_.length - sizeof(CustomPacketHeader));
+            memcpy(data_.data(), data + sizeof(CustomPacketHeader), data_.size());
+        } else {
+            data_.clear();
+        }
+
+        return true;
+    }
+
+    size_t getDataSizeImpl() const {
+        return header_.length;
+    }
+
+private:
+    CustomPacketHeader header_;
+    std::vector<uint8_t> data_;
+};
+
+} // namespace netp 
---
+++ samples/custom/main.cpp
@@ -0,0 +1,302 @@
+#include <iostream>
+#include <thread>
+#include <map>
+#include <netp.h>
+#include "custom_packet.hpp"
+
+using namespace netp;
+
+class CustomServer {
+public:
+    CustomServer(const std::string& ip, uint16_t port) : port_(port) {
+        server_ = createServer();
+        server_->setConnectionHandler([this](ConnectionPtr conn) { onAccept(conn); });
+        server_->setErrorHandler([](const std::string& error) {
+            std::cout << "Server error: " << error << std::endl;
+        });
+    }
+
+    bool start() {
+        return server_->start(port_);
+    }
+
+    void stop() {
+        server_->stop();
+    }
+
+protected:
+    void onAccept(ConnectionPtr socket) {
+        std::cout << "New client connected from " << socket->getRemoteAddress() << std::endl;
+        clients_[socket] = 0; // Initialize with ID 0, will be set when client sends HELLO
+
+        socket->setPacketHandler([this, socket](const std::vector<uint8_t>& data) {
+            onReceive(socket, data);
+        });
+
+        socket->setDisconnectHandler([this, socket]() {
+            onClose(socket);
+        });
+    }
+
+    void onReceive(ConnectionPtr socket, const std::vector<uint8_t>& data) {
+        CustomPacket packet;
+        
+        // Try to deserialize the packet
+        if (!packet.deserialize(data.data(), data.size())) {
+            std::cout << "Invalid packet received, terminating client" << std::endl;
+            
+            // Send KILL command before termination
+            CustomPacket killPacket(0, CustomCommand::KILL);
+            std::vector<uint8_t> killData = {'I', 'n', 'v', 'a', 'l', 'i', 'd', ' ', 'p', 'a', 'c', 'k', 'e', 't'};
+            killPacket.setData(killData);
+            socket->sendPacket(killPacket);
+            
+            socket->disconnect();
+            clients_.erase(socket);
+            return;
+        }
+
+        // Process the packet based on command
+        switch (packet.getCommand()) {
+            case CustomCommand::HELLO: {
+                // Store client ID
+                clients_[socket] = packet.getId();
+                std::cout << "Client " << packet.getId() << " registered" << std::endl;
+                
+                // Send acknowledgment
+                CustomPacket response(packet.getId(), CustomCommand::HELLO);
+                socket->sendPacket(response);
+                break;
+            }
+            
+            case CustomCommand::DATA: {
+                // Handle data differently based on client ID
+                uint32_t clientId = packet.getId();
+                std::cout << "Received DATA from client " << clientId << std::endl;
+                
+                CustomPacket response(clientId, CustomCommand::DATA);
+                std::vector<uint8_t> responseData;
+                
+                // Different behavior based on client ID
+                if (clientId % 2 == 0) {
+                    // For even IDs, echo the data back
+                    responseData = packet.getData();
+                } else {
+                    // For odd IDs, reverse the data
+                    responseData = packet.getData();
+                    std::reverse(responseData.begin(), responseData.end());
+                }
+                
+                response.setData(responseData);
+                socket->sendPacket(response);
+                break;
+            }
+            
+            default: {
+                // Handle undefined command
+                std::cout << "Undefined command received: " << static_cast<uint32_t>(packet.getCommand()) << std::endl;
+                
+                CustomPacket warning(packet.getId(), CustomCommand::WARNING);
+                std::vector<uint8_t> warningData = {'U', 'n', 'd', 'e', 'f', 'i', 'n', 'e', 'd', ' ', 'c', 'm', 'd'};
+                warning.setData(warningData);
+                socket->sendPacket(warning);
+                break;
+            }
+        }
+    }
+
+    void onClose(ConnectionPtr socket) {
+        std::cout << "Client " << clients_[socket] << " disconnected" << std::endl;
+        clients_.erase(socket);
+    }
+
+private:
+    std::unique_ptr<Server> server_;
+    uint16_t port_;
+    std::map<ConnectionPtr, uint32_t> clients_;
+};
+
+class CustomClient {
+public:
+    CustomClient(uint32_t id) : id_(id) {
+        client_ = createClient();
+    }
+
+    bool connect(const std::string& ip, uint16_t port) {
+        if (!client_->connect(ip, port)) {
+            return false;
+        }
+
+        auto conn = client_->getConnection();
+        conn->setPacketHandler([this](const std::vector<uint8_t>& data) {
+            onReceive(data);
+        });
+
+        conn->setDisconnectHandler([this]() {
+            onClose();
+        });
+
+        onConnect();
+        return true;
+    }
+
+    void disconnect() {
+        if (client_ && client_->isConnected()) {
+            client_->disconnect();
+        }
+    }
+
+    void sendHello() {
+        CustomPacket packet(id_, CustomCommand::HELLO);
+        sendPacket(packet);
+    }
+
+    void sendData(const std::vector<uint8_t>& data) {
+        CustomPacket packet(id_, CustomCommand::DATA);
+        packet.setData(data);
+        sendPacket(packet);
+    }
+
+    void sendInvalidCommand() {
+        CustomPacket packet(id_, CustomCommand::UNDEFINED);
+        sendPacket(packet);
+    }
+
+    void sendInvalidPacket() {
+        // Send raw bytes that don't follow the protocol
+        std::vector<uint8_t> invalid = {1, 2, 3, 4};
+        if (client_ && client_->isConnected()) {
+            client_->getConnection()->sendRawData(invalid);
+        }
+    }
+
+protected:
+    void sendPacket(const CustomPacket& packet) {
+        if (client_ && client_->isConnected()) {
+            client_->getConnection()->sendPacket(packet);
+        }
+    }
+
+    void onReceive(const std::vector<uint8_t>& data) {
+        CustomPacket packet;
+        if (!packet.deserialize(data.data(), data.size())) {
+            std::cout << "Client received invalid packet" << std::endl;
+            return;
+        }
+
+        switch (packet.getCommand()) {
+            case CustomCommand::HELLO:
+                std::cout << "Client " << id_ << " received HELLO response" << std::endl;
+                break;
+            
+            case CustomCommand::DATA:
+                std::cout << "Client " << id_ << " received DATA response" << std::endl;
+                break;
+            
+            case CustomCommand::WARNING:
+                std::cout << "Client " << id_ << " received WARNING: ";
+                for (uint8_t byte : packet.getData()) {
+                    std::cout << static_cast<char>(byte);
+                }
+                std::cout << std::endl;
+                break;
+            
+            case CustomCommand::KILL:
+                std::cout << "Client " << id_ << " received KILL command: ";
+                for (uint8_t byte : packet.getData()) {
+                    std::cout << static_cast<char>(byte);
+                }
+                std::cout << std::endl;
+                disconnect();
+                break;
+        }
+    }
+
+    void onConnect() {
+        std::cout << "Client " << id_ << " connected" << std::endl;
+        sendHello();
+    }
+
+    void onClose() {
+        std::cout << "Client " << id_ << " disconnected" << std::endl;
+    }
+
+private:
+    uint32_t id_;
+    std::unique_ptr<Client> client_;
+};
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " [server|client] [client_id]" << std::endl;
+        return 1;
+    }
+
+    std::string mode = argv[1];
+    const std::string ip = "127.0.0.1";
+    const uint16_t port = 12345;
+
+    if (mode == "server") {
+        CustomServer server(ip, port);
+        if (!server.start()) {
+            std::cout << "Failed to start server" << std::endl;
+            return 1;
+        }
+        
+        std::cout << "Server started on " << ip << ":" << port << std::endl;
+        std::cout << "Press Enter to exit..." << std::endl;
+        std::cin.get();
+        
+        server.stop();
+    }
+    else if (mode == "client") {
+        if (argc < 3) {
+            std::cout << "Client ID required" << std::endl;
+            return 1;
+        }
+
+        uint32_t clientId = std::stoul(argv[2]);
+        CustomClient client(clientId);
+        
+        if (!client.connect(ip, port)) {
+            std::cout << "Failed to connect to server" << std::endl;
+            return 1;
+        }
+
+        // Test different scenarios
+        std::cout << "1. Send data" << std::endl;
+        std::cout << "2. Send invalid command" << std::endl;
+        std::cout << "3. Send invalid packet" << std::endl;
+        std::cout << "4. Exit" << std::endl;
+
+        while (true) {
+            int choice;
+            std::cout << "\nEnter choice: ";
+            std::cin >> choice;
+
+            switch (choice) {
+                case 1: {
+                    std::vector<uint8_t> testData = {'H', 'e', 'l', 'l', 'o'};
+                    client.sendData(testData);
+                    break;
+                }
+                case 2:
+                    client.sendInvalidCommand();
+                    break;
+                case 3:
+                    client.sendInvalidPacket();
+                    break;
+                case 4:
+                    return 0;
+                default:
+                    std::cout << "Invalid choice" << std::endl;
+            }
+        }
+    }
+    else {
+        std::cout << "Invalid mode. Use 'server' or 'client'" << std::endl;
+        return 1;
+    }
+
+    return 0;
+} 
